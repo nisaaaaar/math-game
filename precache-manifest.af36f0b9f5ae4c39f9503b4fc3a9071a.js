902783ed6373fbe56876bcfa38065730bad705cf
Nisar <nisara305@gmail.com>
2020-07-26 10:45:48 -0500

    Updates

---
+++ precache-manifest.af36f0b9f5ae4c39f9503b4fc3a9071a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c64933fe654d15360e7bbf127c30e15",
+    "url": "/math-game/index.html"
+  },
+  {
+    "revision": "1eff123db08a095ddb4b",
+    "url": "/math-game/static/css/2.9a78915f.chunk.css"
+  },
+  {
+    "revision": "22ed2d42b8b4905a58d2",
+    "url": "/math-game/static/css/main.94cc21e7.chunk.css"
+  },
+  {
+    "revision": "1eff123db08a095ddb4b",
+    "url": "/math-game/static/js/2.5ecad273.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/math-game/static/js/2.5ecad273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ed2d42b8b4905a58d2",
+    "url": "/math-game/static/js/main.50773d2c.chunk.js"
+  },
+  {
+    "revision": "951f18bc4e115f9eb389",
+    "url": "/math-game/static/js/runtime-main.36d01603.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/math-game/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/math-game/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/math-game/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/math-game/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/math-game/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
